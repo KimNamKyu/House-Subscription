04ca78f5917db9aa4cb851a0c6ab70e33d7536e1
KimNamKyu <skarb629@gmail.com>
2021-06-15 16:10:45 +0900

    mock data

--- servers/src/Router/api/apts/index.ts
+++ servers/src/Router/api/apts/index.ts
@@ -1,8 +1,13 @@
 import { FastifyPluginAsync } from "fastify";
+import exData from "../../../exData";
 
 const aptsRoute: FastifyPluginAsync = async (fastify, opts) => {
     fastify.get('/', async (req, reply) => {
-        reply.send('apts')
+        const data = exData.aptInfo;
+        if(!data){
+            reply.send({resultMsg: 'error'})
+        }
+        reply.status(200).send({resultCode: '200', resultData: data})
     })
 }
 
